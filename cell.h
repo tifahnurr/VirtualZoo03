20442dfbdabcaf2c45e6474fa7151ffc5224e49c
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-13 20:41:18 +0700

    Update cell.h

--- cell.h
+++ cell.h
@@ -1,7 +1,13 @@
 using namespace std;
 
+
+/** @class Cell
+  * Kelas Cell untuk membuat cell
+  */
 class Cell{
 	public:
+		/** @brief Constructor.
+		  * Mengkonstruksi cell*/
 		Cell();
 			
 	protected:
