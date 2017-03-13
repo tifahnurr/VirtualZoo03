e0ef861abf0464dbab5b360fa7eaf077565fe51f
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-13 21:08:31 +0700

    Update herbivore.h

--- herbivore.h
+++ herbivore.h
@@ -11,6 +11,10 @@
 
 #include "dietType.h"
 
+/** @class Herbivore
+  * Kelas Herbivore untuk membuat hewan dengan diet type jenis herbivora
+  */
+
 class Herbivore : public DietType{
    public:
      /**@brief Constructor.
@@ -19,11 +23,12 @@ class Herbivore : public DietType{
    
     /**@brief Constructor.
      * Constructor dengan parameter*/
-		/**@param p porsi yang dibutuhkan oleh suatu animal*/
+    /**@param p porsi yang dibutuhkan oleh suatu animal*/
     Herbivore(int p);
     
     /**@brief feed.
-     * Memberi makan hewan berdasarkan jenis makanannya*/
+     * Memberi makan hewan berdasarkan jenis makanannya
+     * @param feed Nilai banyaknya makanan yang diberikan kepada hewan*/
     void Feeds(int feed);
     
 };
