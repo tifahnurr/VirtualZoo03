81baece7faf7f5c2a77e7d3970bf3d79b6d8b1d0
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-13 20:51:36 +0700

    Update facility.h

--- facility.h
+++ facility.h
@@ -1,6 +1,15 @@
+
+/** @class Facility
+  * Kelas Facility untuk membuat fasilitas yang ada di zoo
+  */
+
 class Facility: public Cell{
 	public:
+		/**@brief Constructor.
+		  *Mengkonstruksi kelas Facility */
 		Facility();
+		/**@brief Dtor.
+		  *Menghapus nilai dari kelas facility */
 		~Facility();
 		
 	protected:
